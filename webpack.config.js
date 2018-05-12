318788f2e4d9bebdaa026290b5f31186813eaa41
mg <miaogg_1@sohu.com>
2018-05-12 17:18:38 +0800

    webpack修改

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-const ClenWebpackPlugin  = require("clean-webpack-plugin");
+const ClenWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BomPlugin = require('webpack-utf8-bom');
+
+
+var isProduction = function () {
+    return process.env.NODE_ENV === 'production';
+}
 
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'js/build.js',
-        path: path.resolve(__dirname, 'dist/'),
+        path: path.resolve(__dirname, './web'),
         //所有资源的基础路径必须以/结尾
-        // publicPath:'/'
+        publicPath:'/',
+        filename: !isProduction() ? 'assets/js/[name]-main.js' : 'assets/js/[name]-main-[hash:6].js',
+        chunkFilename: 'assets/js/[name]-[chunkhash:8]-chunk.js'
     },
     mode: 'development',
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: '../index.html',
-            template: 'src/assets/index.html'
-        }),
-        new ClenWebpackPlugin(['dist']),
-        new webpack.ProvidePlugin({
-            $:'jqurey'
-        })
-    ],
     module: {
         rules: [
             {
@@ -34,113 +32,101 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            //开启CSS模块化
-                            module: true,
-                            localIndentName: '[name]-[local]_[hash:base64:6]'
-                        }
-                    }
-                ],
-                exclude: [
-                    path.resolve(__dirname, 'node_module'),
-                    path.resolve(__dirname, 'src/assets/css')
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-                include: [
-                    path.resolve(__dirname, 'node_module'),
-                    path.resolve(__dirname, 'src/assets/css')
-                ]
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader']
+                }),
+
             },
             {
                 test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            //开启CSS模块化
-                            module: true,
-                            localIndentName: '[name]-[local]_[hash:base64:6]'
-                        }
-                    },
-                    'sass-loader'
-                ],
-                exclude: [
-                    path.resolve(__dirname, 'node_module'),
-                    path.resolve(__dirname, 'src/assets/css')
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader','sass-loader'],
-                include: [
-                    path.resolve(__dirname, 'node_module'),
-                    path.resolve(__dirname, 'src/assets/css')
-                ]
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader', 'sass-loader']
+                }),
             },
             {
                 test: /\.less$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            //开启CSS模块化
-                            module: true,
-                            localIndentName: '[name]-[local]_[hash:base64:6]'
-                        }
-                    },
-                    'less-loader'
-                ],
-                exclude: [
-                    path.resolve(__dirname, 'node_module'),
-                    path.resolve(__dirname, 'src/assets/css')
-                ]
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader', 'less-loader']
+                }),
             },
             {
-                test: /\.less$/,
-                use: ['style-loader', 'css-loader','less-loader'],
-                include: [
-                    path.resolve(__dirname, 'node_module'),
-                    path.resolve(__dirname, 'src/assets/css')
-                ]
-            },
-            {
-                test: /\.(jpg|png|gif|jpeg)$/,
+                test: /\.(jpe?g|png|gif|svg)$/,
                 use: [{
                     loader: 'url-loader',
                     options: {
                         //图片大小限制是否使用base64
-                        limit: 10000,
-                        name:'assets/images/[name]_[hash:8].[ext]'
+                        limit: 1024,
+                        name: 'assets/images/[name]_[hash:8].[ext]'
                     }
                 }]
             },
             {
                 test: /\.(ttf|eot|woff|woff2|svg)$/,
-                use:[{
-                    loader:'file-loader',
-                    options:{
-                        name:'assets/fonts/[name]_[hash:8].[ext]'
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: 'assets/fonts/[name]_[hash:8].[ext]'
                     }
                 }]
-            }
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: false,
+                        removeComments: false,
+                        collapseWhitespace: false
+                    }
+                }
+                ]
+            },
         ]
     },
+    resolve: {
+        extensions: ['.js', '.json'],
+        modules: [
+            'node_modules',
+            path.resolve(__dirname, './src/libs'),
+        ],
+        // alias: {
+        //     layer: path.resolve(__dirname, './node_modules/layer/src/mobile/layer'),
+        //     layercss: path.resolve(__dirname, './node_modules/layer/src/mobile/need/layer.css')
+        // }
+    },
+    plugins: [
+        new ExtractTextPlugin(!isProduction() ? 'assets/css/[name]-main.css' : 'assets/css/[name]-main-[contenthash:6].css'),
+        //解决中文乱码的问题
+        new BomPlugin(true, /\.(html)$/),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/assets/index.html'
+        }),
+        new ClenWebpackPlugin(
+            ['css/*.*', 'js/*.*','images/*.*'], 　 //匹配删除的文件
+            {
+                root: path.resolve(__dirname, './web/assets'),//根目录
+                verbose: true,//开启在控制台输出信息
+                dry: false,//启用删除文件,
+                exclude:[],
+                watch:true
+            }
+        ),
+        new webpack.ProvidePlugin({
+            $: 'jqurey'
+        })
+    ],
     //配置webserver
-    devServer: {
-        //自动打开浏览器
-        open: true,
-        port: 9000,
-        contentBase:'./src/assets',
-        //服务器打包后资源输出的路径
-        publicPath:'/'
-    }
+    // devServer: {
+    //     //自动打开浏览器
+    //     open: true,
+    //     port: 9000,
+    //     contentBase: './src/assets',
+    //     //服务器打包后资源输出的路径
+    //     publicPath: '/'
+    // }
+    devtool: "source-map"
 }
